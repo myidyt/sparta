f59df6b0b2b1c450a4448548abcd39fc037754c4
myidyt <64938742+myidyt@users.noreply.github.com>
2020-06-05 09:48:45 +0900

    add

--- 4일차/homework/app.py
+++ 4일차/homework/app.py
@@ -9,29 +9,27 @@ db = client.dbsparta
 def home():
     return render_template('index.html')
 
-@app.route('/orders', methods=['GET'])
+@app.route('/order', methods = ['GET'])
 def read_order():
     orders = list(db.orders.find({},{'_id':0}))
     return jsonify({'result':'success', 'orders':orders})
 
-@app.route('/orders', methods=['POST'])
+@app.route('/order', methods = ['POST'])
 def write_order():
-    name_receive = request.form['name_give']
-    qty_receive = request.form['qty_give']
-    address_receive = request.form['address_give']
-    phone_receive = request.form['phone_give']
-   
-    doc = {
-        'name': name_receive, 
-        'qty': qty_receive,
-        'address': address_receive, 
-        'phone': phone_receive
-    }
-    db.orders.insert_one(doc)
+    receive_name = request.form['name_give']
+    receive_qty = request.form['qty_give']
+    receive_address = request.form['address_give']
+    receive_phone = request.form['phone_give']
 
-    return jsonify({'result': 'success','msg':'주문이 완료되었습니다.'})
+    order = {
+    'name' : receive_name,
+    'qty' : receive_qty,
+    'address' : receive_address,
+    'phone' : receive_phone
+    }
+    db.orders.insert_one(order)
+
+    return jsonify({'result':'success', 'msg':'주문이 완료되었습니다!'})
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5000, debug=True)
-
-   
+    app.run('0.0.0.0', port=5000, debug=True)
